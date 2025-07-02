8e8b90028003bc87bced4c8dbeeb28de1fac2d02
malachi <abbodthaaer@gmail.com>
2025-07-02 16:39:25 +0300

    add glfw and glad linter detection with window

--- build_exec.sh
+++ build_exec.sh
@@ -2,4 +2,4 @@
 for dir in vendor/*/include; do
   INCLUDE_PATHS="$INCLUDE_PATHS -I$dir"
 done
-g++ src/*.cpp deps/*.a -o YOUR_APP ${INCLUDE_PATHS} -lGL -ldl -lpthread -lm -std=c++17 -Wall -Wextra -Wpedantic -O3 #max optimizations
+g++ src/*.cpp src/glad.c deps/*.a ${INCLUDE_PATHS} -lGL -ldl -lpthread -lm -std=c++17 -Wall -Wextra -Wpedantic -O3 -o run #max optimizations
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,62 @@
 #include <cstdlib> //C standard lib, handy
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 
-#include "test.h"
-
-int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
+//tells OpenGL we resized
+void framebuffer_size_callback(GLFWwindow*, int width, int height)
 {
-    YOUR_APP::Message();
-    return EXIT_SUCCESS;
+    glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow* window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+int main()
+{
+    //window instantiation
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    //window object
+    GLFWwindow* window = glfwCreateWindow(800, 600, "success" , NULL, NULL);
+    if (window == NULL)
+    {
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    //tell OpenGL this is where you're working, slave
+    glfwMakeContextCurrent(window);
+
+    //loads OpenGL function pointers (bridges GLFW and OpenGL)
+    if(!gladLoadGL())
+        return EXIT_FAILURE;
+
+    //OpenGL playground
+//    glViewport(0, 0, 800, 600);
+
+    //resize callback function call
+//   glfwSetFramebufferSizeCallback(window, &framebuffer_size_callback);
+
+    //render loop
+    while(!glfwWindowShouldClose(window))
+    {
+        process_input(window);
+
+        glClearColor(0.5f, 0.1f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    //self explanatory
+    glfwTerminate();
+
+    return 0;
 }
